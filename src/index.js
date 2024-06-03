fe6215ad382147c6e7be3df141a7e4911d376b34
FRANCO IAIR BRONSZTEIN CASTELLAN <47344052@ort>
2024-06-03 12:13:29 -0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 // En postman se usa "localhost:3000"
+// instañar npm install tedious 
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'A-PHZ2-CIDI-29',  //A-PHZ2-CIDI-29   Nombre de compu
+        authentication: {
+            type: 'default',
+            options: {
+              trustedConnection: true
+            }
+        }
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.
+        console.log("Connected");  
+    });
+    
+    connection.connect();
 
-import Tienda from './Models/Tienda'
+
+
+
+import Tienda from './Models/Tienda' 
 import express from "express"; // hacer npm i express
 import cors from "cors"; // hacer npm i cors
 const app = express();
