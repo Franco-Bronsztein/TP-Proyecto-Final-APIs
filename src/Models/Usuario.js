8015a6badcc84555880b4d719ec31d30c8a5e207
FRANCO IAIR BRONSZTEIN CASTELLAN <47344052@ort>
2024-06-04 07:53:45 -0300

    Models Usuario

--- src/Models/Usuario.js
+++ src/Models/Usuario.js
@@ -1,12 +1,15 @@
 export default class Usuario {
-    constructor(id,nombre,direccion,foto,cantestrellas){
+    constructor(id,nombre,apellido,telefono,mail,contraseña,vendedor){
         this.id = id;
         this.nombre = nombre;
-        this.direccoin = direccion;
-        this.foto = foto;
-        this.cantestrellas = cantestrellas;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.mail = mail;
+        this.contraseña = contraseña;
+        this.vendedor = vendedor;
+
     }
     toString() {
-        return `Nombre: ${this.nombre}, Id: ${this.id}, Direccion: ${this.direccion}, Foto: ${this.foto}, Cantestrellas: ${this.cantestrellas}`
+        return `Nombre: ${this.nombre}, Id: ${this.id}, Apellido: ${this.apellido}, Telefono: ${this.telefono}, Mail: ${this.mail},Contraseña ${this.contraseña},Vendedor ${this.vendedor}`
     }
 }
