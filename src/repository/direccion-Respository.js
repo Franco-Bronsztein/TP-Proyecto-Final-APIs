d2884ac137464f212ec9f2f1a538549e64e4b7ed
Android Studio <devandroid@ort>
2024-06-14 09:37:21 -0300

    APIS HECHAS

--- src/repository/direccion-Respository.js
+++ src/repository/direccion-Respository.js
@@ -8,10 +8,12 @@ export default class direccionRepository {
     getByIdAsync = async (idusuario) => {
         let returnArray = null;
         const client = new Client(DBConfig);
+        console.log(idusuario)
         try {
             await client.connect();
             const sql = `SELECT ubicacion FROM direccion WHERE idusuario = $1`;
             const values = [idusuario]
+            console.log(values)
             const result = await client.query(sql,values);
             await client.end();
             returnArray = result.rows;
--- src/service/direccion-service.js
+++ src/service/direccion-service.js
@@ -3,12 +3,7 @@ import direccionRepository from '../repository/direccion-Respository.js'
 export default class direccionService{
     getByIdAsync = async (idusuario) => {
         const repo = new direccionRepository();
-        const returnArray = await repo.getByIdAsync();
-        return returnArray;
-    }
-     getAllByNameAsync = async () => {
-        const repo = new direccionRepository();
-        const returnArray = await repo.getAllByNameAsync(id);
+        const returnArray = await repo.getByIdAsync(idusuario);
         return returnArray;
     }
 }
