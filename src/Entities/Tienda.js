e4a514473d8957b855659d2fa05eb780584c31c9
Android Studio <devandroid@ort>
2024-07-01 12:07:39 -0300

    :

--- src/Entities/Tienda.js
+++ src/Entities/Tienda.js
@@ -1,13 +1,14 @@
 export default class Tienda {
-    constructor(id, nombre, direccion, foto, cantestrellas){
+    constructor(id, nombre, direccion, foto, cantestrellas, confiteria){
         this.id = id;
         this.nombre = nombre;
         this.direccion = direccion; 
         this.foto = foto;
         this.cantestrellas = cantestrellas;
+        this.confiteria = confiteria;
     }
 
     toString() {
-        return `Nombre: ${this.nombre}, Id: ${this.id}, Dirección: ${this.direccion}, Foto: ${this.foto}, Cantestrellas: ${this.cantestrellas}`;
+        return `Nombre: ${this.nombre}, Id: ${this.id}, Dirección: ${this.direccion}, Foto: ${this.foto}, Cantestrellas: ${this.cantestrellas} , Confiteria: ${this.confiteria}`;
     }
 }
