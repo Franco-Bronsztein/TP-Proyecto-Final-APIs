7109bb29fcadfe2614621d88e5ab97c3b5aedbab
Android Studio <devandroid@ort>
2024-07-08 09:45:37 -0300

    API RECOMENDADOS

--- src/repository/recomendados-Repository.js
+++ src/repository/recomendados-Repository.js
@@ -11,8 +11,10 @@ export default class recomendadosRepository {
         console.log(idusuario)
         try {
             await client.connect();
-            const sql = `SELECT L.nombre,L.direccion,L.foto,L.cantestrellas FROM local L WHERE L.id = R.id AND R.idusuario = $idusuario
-            join Recomendados R on  L.id = R.idlocal`;
+            const sql = `SELECT L.nombre, L.direccion, L.foto, L.cantestrellas 
+            FROM local L 
+            JOIN public."Recomendados" R ON L.id = R.idlocal 
+            WHERE R.idusuario = 1;`;
             const values = [idusuario]
             console.log(values)
             const result = await client.query(sql,values);
