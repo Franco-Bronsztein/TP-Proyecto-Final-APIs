b1daffe492acc79688ba07264c8327e74863ead4
Android Studio <devandroid@ort>
2024-07-29 12:04:25 -0300

    API CONFIRMAR PEDIDO

--- src/service/pedido-service.js
+++ src/service/pedido-service.js
@@ -1,9 +1,9 @@
 import pedidoRepository from '../repository/pedido-Repository.js'
 
 export default class PedidoService {
-    createPedidoAsync = async (idusuario, productos, total) => {
+    createPedidoAsync = async (idProducto,idLocal,idUsuario,idPedido) => {
         const repo = new pedidoRepository();
-        const result = await repo.createPedidoAsync(idusuario, productos, total);
+        const result = await repo.createPedidoAsync(idProducto,idLocal,idUsuario,idPedido);
         return result;
     };
 }
