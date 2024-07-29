bac412a1ca1e8124ba0523a76d40d7b8f97c5082
Android Studio <devandroid@ort>
2024-07-29 12:03:21 -0300

    API CONFIRMAR PEDIDO

--- src/Controllers/Pedido-Controller.js
+++ src/Controllers/Pedido-Controller.js
@@ -4,9 +4,8 @@ import PedidoService from '../service/pedido-service.js'
 const router = Router();
 const svc =  new PedidoService();
 router.post('/', async (req, res) => {
-    const { idusuario, productos, total } = req.body; 
     try {
-        const result = await svc.createPedidoAsync(idusuario, productos, total);
+        const result = await svc.createPedidoAsync(req.body);
         console.log(result)
         if (result) {
             res.status(200).json(result);
--- src/repository/pedido-Repository.js
+++ src/repository/pedido-Repository.js
@@ -5,13 +5,13 @@ import pkg from 'pg'
 const { Client, Pool } = pkg;
 
 export default class PedidoRepository {
-    createPedidoAsync = async (idusuario, productos, total) => {
+    createPedidoAsync = async (idProducto,idLocal,idUsuario,idPedido) => {
         let returnArray = null;
         const client = new Client(DBConfig);
         try {
             await client.connect();
-            const sql = 'INSERT INTO ';
-            const values = [idusuario, JSON.stringify(productos), total];
+            const sql = 'INSERT INTO public."detallePedido" (FK_IDlocal,FK_IDUsuario,FK_IDProducto,FK_IDpedido) VALUES ($idLocal,$idUsuario,$idProducto,$idPedido)';
+            const values = [idProducto,idLocal,idUsuario,idPedido];
             const result = await client.query(sql, values);
             await client.end();
             returnArray = result.rows;
