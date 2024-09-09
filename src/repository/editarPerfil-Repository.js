a462ce3ae19b27dfbab0a7a1f06f5e0e85c38b65
Android Studio <devandroid@ort>
2024-09-09 11:12:44 -0300

    a

--- src/repository/editarPerfil-Repository.js
+++ src/repository/editarPerfil-Repository.js
@@ -9,13 +9,13 @@ export default class editarPerfilRepository {
         const client = new Client(DBConfig);
         try {
             await client.connect();
+            console.log(userData)
             const sql = `UPDATE public."usuario"
-                         SET "nombre" = $1, "apellido" = $2, "telefono" = $3, "password" = $4, "vendedor" = $5, "username" = $6, "fechaNac" = $7, "fotoPerfil" = $8
-                         WHERE "mail" = $9;`;
-            const values = [userData.nombre, userData.apellido, userData.telefono, userData.password, userData.vendedor, userData.username, userData.fechaNac, userData.fotoPerfil, userData.mail];
-            const result = await client.query(sql, values);
+            SET "nombre" = '${userData.nombre}', "apellido" = '${userData.apellido}', "telefono" = ${userData.telefono}, "password" = '${userData.password}', "vendedor" = ${userData.vendedor}, "username" = '${userData.username}', "fechaNac" = '${userData.fechaNac}', "fotoPerfil" = '${userData.fotoPerfil}', "mail" = '${userData.mail}'
+            WHERE "id"= ${userData.id} ;`
+            const result = await client.query(sql);
             await client.end();
-            returnArray = result.rowCount;
+            returnArray = result.rows;
         } catch (error) {
             console.log(error);
         }
