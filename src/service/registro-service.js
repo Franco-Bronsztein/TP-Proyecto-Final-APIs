b70494b0405a65b2b014966772cd3bf70b568b05
Android Studio <devandroid@ort>
2024-09-09 08:47:55 -0300

    no email

--- src/service/registro-service.js
+++ src/service/registro-service.js
@@ -1,5 +1,4 @@
 import registroRepository from '../repository/registro-Repository.js';
-import { SMTPClient } from 'smtp-client';
 
 export default class registroService {
     registrarAsync = async (registroInfo) => {
@@ -19,37 +18,8 @@ export default class registroService {
 
         // Registrar el nuevo usuario
         const result = await repo.registrarseAsync(registroInfo);
-
-        if (result) {
-            // Si el registro fue exitoso, enviar un correo de bienvenida
-            await this.enviarCorreoRegistro(registroInfo.mail);
-        }
-
         return result;
     };
 
-    enviarCorreoRegistro = async (correoUsuario) => {
-        const client = new SMTPClient({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            timeout: 60000,
-        });
 
-        try {
-            await client.connect();
-            await client.greet({ hostname: 'localhost' });
-            await client.authLogin({
-                username: 'francobronsztein@gmail.com',
-                password: 'mhpa icuc tzlm jrid',
-            });
-            await client.mail({ from: 'francobronsztein@gmail.com' });
-            await client.rcpt({ to: correoUsuario });
-            await client.data(`Subject: ¡Bienvenido a nuestra app!\nTo: ${correoUsuario}\nContent-Type: text/html; charset=utf-8\n\n<h1>¡Bienvenido!</h1><p>Gracias por registrarte en nuestra aplicación. Estamos encantados de tenerte con nosotros!!</p>`);
-            await client.quit();
-            console.log('Correo enviado con éxito a:', correoUsuario);
-        } catch (error) {
-            console.error('Error al enviar el correo:', error);
-        }
-    };
 }
